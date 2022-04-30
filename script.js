9e2e2e78901db348b2c9b82f58da47bfd08e9e07
ExQRFx <exqr.fx@gmail.com>
2022-04-30 18:41:58 +0500

    buttons show numbers on display, working clear and backspace buttons

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function divide(a,b) {
     return a/b
 }
 
+const numButtons = document.querySelectorAll('.button.number');
+const display = document.querySelector('#display');
+numButtons.forEach(button => button.addEventListener('click', (e) => {
+    display.innerHTML += e.target.innerHTML;
+}));
+
+const clearButton = document.querySelector('.button.clear');
+clearButton.addEventListener('click', (e) => {
+    display.innerHTML = '';
+});
+
+const backSpace = document.querySelector('.button.backspace');
+backSpace.addEventListener('click', (e) => {
+    let displayText = [...display.innerHTML];
+    displayText.pop();
+    display.innerHTML = displayText.join('');
+});
+
