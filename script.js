da1a144af883175fd37d2b07d208bd35f6234826
ExQRFx <exqr.fx@gmail.com>
2022-04-30 21:52:42 +0500

    functionality finished

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function multiply(a,b) {
 }
 function divide(a,b) {
     if (b === 0) {
-        return "Division by zero"
+        return "To the infinity!"
     }
     return a/b
 }
@@ -27,7 +27,6 @@ function operate(operator, a, b) {
     }
 }
 
-
 const numButtons = document.querySelectorAll('.button.number');
 const display = document.querySelector('#display');
 numButtons.forEach(button => button.addEventListener('click', (e) => {
@@ -37,6 +36,7 @@ numButtons.forEach(button => button.addEventListener('click', (e) => {
 const clearButton = document.querySelector('.button.clear');
 clearButton.addEventListener('click', (e) => {
     display.innerHTML = '';
+    countClick = 0;
 });
 
 const backSpace = document.querySelector('.button.backspace');
@@ -48,22 +48,58 @@ backSpace.addEventListener('click', (e) => {
 
 let currOperator = '';
 let currNumber;
-let isOperatorPressed = false;
+let countClick = 0;
+let countDotClick = 0;
 
 const btnOperators = document.querySelectorAll('.button.operator');
 btnOperators.forEach(button => button.addEventListener('click', (e) => {
-    currNumber = Number(display.innerHTML);
-    currOperator = e.target.innerHTML;
-    display.innerHTML += e.target.innerHTML;
-    isOperatorPressed = true;
+    countDotClick = 0;
+    if (countClick === 0) {
+        countClick++;
+        currNumber = Number(display.innerHTML);
+        currOperator = e.target.innerHTML;
+        display.innerHTML += e.target.innerHTML;   
+    } else {
+        performCalc();
+        currNumber = Number(display.innerHTML);
+        currOperator = e.target.innerHTML;
+        display.innerHTML += e.target.innerHTML;
+    }
 }));
 
 const btnEquals = document.querySelector('.button.equals');
 btnEquals.addEventListener('click', () => {
-    let secondNumber = splitDisplay(display);
-    display.innerHTML = operate(currOperator, currNumber, secondNumber);
+    if (display.innerHTML !== '') {
+        performCalc();
+        countClick = 0;
+    }
 });
 
-function splitDisplay(display) {
-    return Number(display.innerHTML.split(/[*/+-]/g)[1]);
+
+function performCalc() {
+    let secondNumber = splitDisplay(display);
+    display.innerHTML = operate(currOperator, currNumber, secondNumber);
 }
+
+function splitDisplay(display) {
+    return Number(display.innerHTML.split(/[*/+-]/g).slice(-1)[0]);
+}
+
+const dotButton = document.querySelector('.button.dot');
+dotButton.addEventListener('click', (e) => {
+    countDotClick++;
+    if (countDotClick === 1) {
+        display.innerHTML += e.target.innerHTML;
+    } 
+})
+
+const negateButton = document.querySelector('.button.negate');
+negateButton.addEventListener('click', (e) => {
+    let displayText = [...display.innerHTML];
+    if (displayText[0] === '-') {
+        displayText.shift();
+    } else {
+        displayText.unshift('-');
+    }
+    display.innerHTML = displayText.join('');
+});
